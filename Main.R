53909f0b8e4d2e101e89b267ce7d3ecdb534ed51
Juan Pablo Carreon Hidalgo <jpch_26@outlook.com>
2021-01-22 16:57:29 -0600

    output_dir on render function

--- Main.R
+++ Main.R
@@ -40,8 +40,8 @@ source("analysis/bar_point_graph.R")
 
 # 7 Source presentation document files. You need 'rmarkdown' package.
 
-rmarkdown::render("github_document.Rmd")
-rmarkdown::render("pdf_document.Rmd")
+rmarkdown::render("github_document.Rmd", output_dir = "document_presentation")
+rmarkdown::render("pdf_document.Rmd", output_dir = "document_presentation")
 
 # 8 Session Info 
 
